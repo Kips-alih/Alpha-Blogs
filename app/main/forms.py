2b8b954a4221bacbd17792d985e991c5e618963b
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-15 08:19:56 +0300

    Create the form modules

---
+++ app/main/forms.py
@@ -0,0 +1,19 @@
+from wtforms import StringField,TextAreaField, SubmitField, SelectField
+from wtforms.validators import Required
+from flask_wtf import FlaskForm
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+class BlogForm(FlaskForm):
+    title = StringField('Blog title', validators=[Required()])
+    category = SelectField('Blog category',choices=[('Choose category','Choose category'),('Lifestyle', 'Lifestyle'),('News','News'),('Sports','Sports'),('Politics','Politics'),('Fashion','Fashion'),('Travel', 'Travel')], validators=[Required()])
+    description = StringField('What is your blog?')
+    submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+    comment = StringField('Comment here', validators=[Required()])
+    submit = SubmitField('Submit')
