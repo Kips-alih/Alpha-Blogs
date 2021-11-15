870d1bb29db0a0ca9e8df2b5d693647373baefb3
Kipngeno <alphonce.kipngeno@student.moringaschool.com>
2021-11-15 08:40:48 +0300

    Define other routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,11 @@
-from flask import render_template,request,redirect,url_for,abort
+from flask import render_template,request,redirect,url_for,abort,flash
 
-from app.main.forms import UpdateProfile
+from app.main.forms import UpdateProfile,BlogForm,CommentForm
 from . import main
-from ..models import  User
+from ..models import  User,Blog,Comment
 from flask_login import login_required,current_user
 from .. import db,photos
-
-
+from ..request import get_quotes
 
 
 #views
@@ -18,10 +17,82 @@ def index():
 
     title = 'Home - Welcome to Alpha Blogs App'
 
+    quote = get_quotes()
+    blogs = Blog.query.order_by(Blog.date_posted).all()
+
     
-    return render_template('index.html', title = title,)
+    return render_template('index.html', title = title,quote=quote,blogs=blogs)
 
 
+@main.route('/user_blog', methods=['GET', 'POST'])
+@login_required
+def user_blog():
+    blog_form = BlogForm()
+
+    if blog_form.validate_on_submit():
+        title = blog_form.title.data
+        category=blog_form.category.data
+        description=blog_form.description.data
+        user_blog = Blog(title=title,category=category,description=description,user=current_user)
+
+        user_blog.save_blog()
+        db.session.add(user_blog)
+        db.session.commit()
+        
+        return redirect(url_for('main.index'))
+    else:
+        blogs = Blog.query.order_by(Blog.date_posted).all()
+    
+    return render_template('blog.html', blogs=blogs,blog_form = blog_form)
+
+
+
+@main.route('/user_blog/<id>', methods=['GET', 'POST'])
+@login_required
+def blog_comments(id):
+
+    comments = Comment.query.filter_by(blog_id=id).all()
+    blogs = Blog.query.get(id)
+    if blogs is None:
+        abort(404)
+    comment_form = CommentForm()
+    if comment_form.validate_on_submit():
+        comment = Comment( comment=comment_form.comment.data, blog_id=id, user_id=current_user.id)
+        db.session.add(comment)
+        db.session.commit()
+        comment_form.comment.data = ''
+        return redirect(url_for('main.read_comments', id=id))
+
+    return render_template('new_comment.html',blogs= blogs, comments=comments, comment_form = comment_form)
+
+@main.route('/user_blog/<int:id>/delete', methods=['GET', 'POST'])
+@login_required
+def delete_blog(id):
+    """
+    Delete blog function that deletes the blog
+    """
+    blog = Blog.get_blog(id)
+
+    db.session.delete(blog)
+    db.session.commit()
+    return redirect(url_for('main.index', username=current_user.username))
+
+
+
+@main.route('/read_comments/<id>')
+@login_required
+def read_comments(id):
+    comment = Comment.get_comments(id)
+    return render_template('comments.html', comment=comment)
+
+
+@main.route('/delete/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_comment(id):
+    comment =Comment.query.get_or_404(id)
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect (url_for('main.read_comments',id=id))
 
 @main.route('/user/<uname>')
 def profile(uname):
